75844163a52638d75037b24ba753ef43ed782014
stephen <rguerra712@yahoo.com>
2018-06-03 23:24:25 -0500

    Added interface for just order placement since I plan to extend it a few times

--- lib/order-client.ts
+++ lib/order-client.ts
@@ -1,10 +1,9 @@
-import { Order, OrderPlacedResult } from "../types/types";
+import { OrderPlacer } from "./order-placer";
 
 /**
  * A client wrapping an API to place/cancel orders based on their status and reschedule periodically if needed
  */
-export interface OrderClient {
-  placeOrder(order: Order): Promise<OrderPlacedResult>;
+export interface OrderClient extends OrderPlacer{
   isOrderActive(orderId: string): Promise<boolean>;
   cancelOrder(orderId: string): Promise<string[]>;
 }
---
+++ lib/order-placer.ts
@@ -0,0 +1,7 @@
+import { Order, OrderPlacedResult } from "../types/types";
+/**
+ * An interface that incompasses solely the placing of an order
+ */
+export interface OrderPlacer {
+    placeOrder(order: Order): Promise<OrderPlacedResult>;
+}
