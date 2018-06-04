bc4c1f46fc4e4afa164f51267881e67d53a0da25
stephen <rguerra712@yahoo.com>
2018-06-03 21:24:21 -0500

    Finally got mock and test working, both moqito and moq gave issues, turned out it was sourcemap, then it was including done and never calling it, warning for later

--- lib/trader.ts
+++ lib/trader.ts
@@ -1,17 +1,17 @@
-import { OrderPlacedResult, Order } from './../types/types';
+import { OrderPlacedResult, Order } from "./../types/types";
 import { OrderClient } from "./order-client";
 
 export class Trader {
-  //private orderClient: OrderClient;
+  private orderClient: OrderClient;
 
   /**
    * Createa a new instance of a trader that places orders and cancels existing ones
    */
   constructor(orderClient: OrderClient) {
-    //this.orderClient = orderClient;
+    this.orderClient = orderClient;
   }
-  
+
   trade = (order: Order, idToCancel?: string): Promise<OrderPlacedResult> => {
-    return new Promise<OrderPlacedResult>((resolve, reject) => {resolve(new OrderPlacedResult('123'))});
+    return this.orderClient.placeOrder(order);
   };
 }
